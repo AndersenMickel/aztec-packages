40588be681b545ec940cd3bfceacfa803507bcfa
jeanmon <jean@aztecprotocol.com>
2024-08-14 16:49:02 +0000

    7790: WIP - debugging assertion in from_witness()

--- barretenberg/cpp/src/barretenberg/vm/avm/recursion/avm_recursive_flavor.hpp
+++ barretenberg/cpp/src/barretenberg/vm/avm/recursion/avm_recursive_flavor.hpp
@@ -95,7 +95,7 @@ namespace bb {
 template <typename BuilderType> class AvmRecursiveFlavor_ {
 
   public:
-    // much of the types end up being the same but derived from the circuit builer types
+    // much of the types end up being the same but derived from the circuit builder types
     using CircuitBuilder = BuilderType;
     using Curve = stdlib::bn254<CircuitBuilder>;
     using PCS = KZG<Curve>;
@@ -105,7 +105,7 @@ template <typename BuilderType> class AvmRecursiveFlavor_ {
     using BF = typename Curve::BaseField;
 
     using NativeFlavor = AvmFlavor;
-    using NativeVerificationKey = typename NativeFlavor::VerificationKey;
+    using NativeVerificationKey = NativeFlavor::VerificationKey;
 
     using VerifierCommitmentKey = bb::VerifierCommitmentKey<NativeFlavor::Curve>;
 
@@ -161,10 +161,22 @@ template <typename BuilderType> class AvmRecursiveFlavor_ {
             this->log_circuit_size = numeric::get_msb(this->circuit_size);
             // this->num_public_inputs = native_key->num_public_inputs;
             // this->pub_inputs_offset = native_key->pub_inputs_offset;
-
+            info("JEAMON - TESTING");
             for (auto [native_comm, comm] : zip_view(native_key->get_all(), this->get_all())) {
+                info("JEAMON - NATIVE COMMIT:", native_comm);
+                info("JEAMON - INITIAL COMMIT:", comm);
+
+                // Figure out whether the EC relation y^2 = x^3 + 3 (mod. q)
+                fq rhs = (native_comm.x * native_comm.x) * native_comm.x;
+                rhs = rhs + 3;
+                info("RHS: ", rhs);
+                fq lhs = native_comm.y * native_comm.y;
+                info("LHS: ", lhs);
+
                 comm = Commitment::from_witness(builder, native_comm);
+                info("JEAMON - COMMIT:", comm);
             }
+            info("AFTER JEAMON - TESTING");
         }
     };
 
